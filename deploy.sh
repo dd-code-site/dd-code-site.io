07412a853fb1acb3be6add1a378037c7879f9c3c
liudandan <dawson2@qq.com>
2021-04-10 22:11:09 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-git add .
-git commit -m 'deploy'
-git push -u origin main
 # 确保脚本抛出遇到的错误
 set -e
 yarn run build
 # 生成静态文件
 tcb hosting deploy dist /
+git add .
+git commit -m 'deploy'
+git push -u origin main
